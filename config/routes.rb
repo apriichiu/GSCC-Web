53be668ca88e476d485f5ffd010c3e0bd144b196
April <april.y.chiu@gmail.com>
2010-09-08 23:04:49 -0700

    push categories/entries/resources scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,54 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :series
+
+  map.resources :entries
+
+  map.resources :resources
+
+  map.resources :resources
+
+  map.resources :entries
+
+  map.resources :entries
+
+  map.resources :resources
+
+  map.resources :resources
+
+  map.resources :resources
+
+  map.resources :entries
+
+  map.resources :resources
+
+  map.resources :resources
+
+  map.resources :entries
+
+  map.resources :resources
+
+  map.resources :resources
+
+  map.resources :resources
+
+  map.resources :entries
+
+  map.resources :entries
+
+  map.resources :entries
+
+  map.resources :entries
+
+  map.resources :categories
+
+  map.resources :series
+
+  map.resources :resources
+
+  map.resources :entries
+
+  map.resources :series
+
   map.resources :events
   map.resources :sessions
   map.login 'login', :controller => 'sessions', :action => 'new'
