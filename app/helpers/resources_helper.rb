c52392f59eb851858eec699fa40e8016495ab9db
Isaac Liu <isaacliu@isaacmac.gateway.2wire.net>
2010-09-21 00:40:37 -0700

    fixed back link for creating new entry/attachment/folder added the list links to the admin menu

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -6,7 +6,8 @@ template = <<-eos
   <% if flash[:notice] -%> <div id="notice"><%= flash[:notice] %></div>
   <% end -%>
   <% if admin? %>
-  <%= link_to 'New Folder', new_folder_path %> | <%= link_to 'New Entry', new_entry_path %> | <%= link_to 'New Attachment', new_attachment_path %> | <%= link_to 'Logout', logout_path %>
+  <%= link_to 'New Folder', new_folder_path+"?from="+request.env['PATH_INFO'] %> | <%= link_to 'New Entry', new_entry_path+"?from="+request.env['PATH_INFO'] %> | <%= link_to 'New Attachment', new_attachment_path+"?from="+request.env['PATH_INFO'] %> | <%= link_to 'Logout', logout_path %> <br />
+  <%= link_to 'List Folders', "/folders" %> | <%= link_to 'List Entries', "/entries" %> | <%= link_to 'List Attachments', "/attachments" %>
   <% else %>
   <%= link_to 'Login', login_path+"?redirect="+request.env['PATH_INFO'] %> to add/edit <%= params[:controller] %>
   <% end %>
