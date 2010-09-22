3d7d1aee8ec1b3fb11719d885f31bf4e8de5516d
Isaac Liu <isaacliu@isaacMac.local>
2010-09-21 18:17:22 -0700

    added edit and delete images included blanks in the selection boxes for the entry and attachment creation and editing fixed the resource table admin column layout added a migration to add remaining fields to tables ( entries => entry_date, tags, verse | folder => type )

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100919054053) do
+ActiveRecord::Schema.define(:version => 20100922011042) do
 
   create_table "attachments", :force => true do |t|
     t.string   "name"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(:version => 20100919054053) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "folder_id"
+    t.string   "verse"
+    t.datetime "entry_date"
+    t.string   "tags"
   end
 
   create_table "folders", :force => true do |t|
@@ -36,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20100919054053) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "type"
   end
 
 end
